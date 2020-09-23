9f1e1530bf4ac69cdbe06fee87068d0ec11b9e53
DESKTOP-DR4T9QF\kmang <katelyn.mangold@my.simpson.edu>
2020-09-23 15:05:59 -0500

    blah

--- app/src/main/java/com/example/scct/Dashboard.kt
+++ app/src/main/java/com/example/scct/Dashboard.kt
@@ -1,37 +1,29 @@
+package com.example.scct/*
 package com.example.scct
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class Dashboard : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.activity_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-
         view.findViewById<Button>(R.id.button2).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_dashboard1)
-
+            findNavController().navigate(R.id.action_Dashboard_to_FirstFragment)
         }
-
     }
 }
+*/
